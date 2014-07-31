20895c2be5429f2b0b8981a9bab89569ba2b786a
Ivo Georgiev <ivo@linvo.org>
2014-07-31 17:08:58 +0300

    Initial commit

---
+++ generate.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node 
+var _ = require("lodash"),
+	moment = require("moment");
+
+var places = require("./moves/places"),
+	activities = require("./moves/activities");
+
+console.log(places,activities)
