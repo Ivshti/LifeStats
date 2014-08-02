ed92b35b89ddedabf2f3ba7c02c1f2e63ec6351c
Ivo Georgiev <ivo@linvo.org>
2014-08-02 21:07:06 +0300

    Aggregation implemented

--- charts.js
+++ charts.js
@@ -1,3 +1,5 @@
 module.exports = [
-	{ name: "cycling", query: { group: "cycling" }, from: "activities" }
+	{ name: "cycling", query: { group: "cycling" }, from: "activities", aggregate: "distance" },
+	{ name: "Divine", query: { "place.name": "Divine" }, from: "places", aggregate: "duration" }
+
 ];
--- generate.js
+++ generate.js
@@ -1,23 +1,29 @@
 #!/usr/bin/env node 
 var _ = require("lodash"),
-	moment = require("moment");
+	moment = require("moment"),
+	traverse = require("traverse");
 
 var datas = { activities: require("./moves/activities"), places: require("./moves/places") };
 var charts = require("./charts");
 
-// Normalize the data
-/*storyline.forEach(function(elem) {
-	elem.date = elem.date.slice(0,4)+"-"+elem.date.slice(4,6)+"-"+elem.date.slice(6,8);
-});
-*/
-
 var results = [];
 charts.forEach(function(chart) {
 	var data = datas[chart.from].map(function(x) {
+		var filtered = _.filter(x.summary || x.segments, chart.query).map(function(seg) {
+			// add duration to places
+			if (! seg.duration) seg.duration = Math.round((new Date(seg.endTime).getTime() -  new Date(seg.startTime).getTime())/1000);
+			return seg;
+		});
+
 		return { 
 			date: x.date.slice(0,4)+"-"+x.date.slice(4,6)+"-"+x.date.slice(6,8),
-			data: _.filter(x.summary || x.segments, chart.query)
+			data: filtered,
+			result: chart.aggregate && filtered.map(function(x) { return x[chart.aggregate] }).reduce(function(a, b) { return a+b }, 0)
 		}
-	})
-	console.log(data)
+	});
+
+	console.log({ 
+		days: data,
+		overall: data.map(function(x) { return x.result }).reduce(function(a,b) { return a+b }, 0)
+	});
 });
